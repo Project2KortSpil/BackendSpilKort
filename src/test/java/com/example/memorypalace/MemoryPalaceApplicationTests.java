bc80077982a3106a43396815076d6a9e495f324a
Hassan2200dk <hassan.2002@outlook.dk>
2023-11-21 10:58:17 +0100

    Initial commit

---
+++ src/test/java/com/example/memorypalace/MemoryPalaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memorypalace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryPalaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
